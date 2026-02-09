800dd3147db809fcbde6f8e751602a01d5abc3bb
AsimaDzh <animasoon@gmail.com>
2026-02-10 00:31:30 +0600

    Added new folders

---
+++ Assets/_Scripts/Animals/HeartsLookAtCamera.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class HeartsLookAtCamera : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        transform.LookAt(
+            transform.position + Camera.main.transform.rotation * Vector3.forward, 
+            Camera.main.transform.rotation * Vector3.up);
+    }
+}
